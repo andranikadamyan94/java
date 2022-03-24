8c8e8b159c5de431ffe8cd0051d8f2e8a7572723
andranikadamyan94 <andranikadamyan94@gmail.com>
2022-03-24 20:35:58 +0400

    git remote add origin https://github.com/andranikadamyan94/java.git

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = new int[100];
 
-
         for(int i = 0;i<100;i++){
             System.out.println("arr[" + i + "] " + arr[i]);
         }
